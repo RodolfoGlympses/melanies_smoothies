3b3d0d591f37925ec49fdbe0aa36dcc595ce3ebe
Rodolfo Redard <rredard@glympses.cl>
2025-07-22 11:38:47 -0400

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,5 @@ if ingredients_list:
 # New section to display smoothiefroot nutrion information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
