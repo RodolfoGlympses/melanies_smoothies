c315c2a10bc0427675f778117233ea15d6456117
Rodolfo Redard <rredard@glympses.cl>
2025-07-22 11:44:22 -0400

    Update streamlit_app.py
    options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -21,15 +23,13 @@ ingredients_list = st.multiselect(
     , max_selections=5
     )
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""""+name_on_order+"""')"""
@@ -40,8 +40,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
       
-# New section to display smoothiefroot nutrion information
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
